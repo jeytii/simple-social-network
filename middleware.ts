b6147950629479e2cac199d44ddd8780a28aa75b
Jonathan Tarnate <jstarnate16@gmail.com>
2022-08-24 20:50:14 +0800

    Create global middleware

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(req: NextRequest) {
+  const isPublic = ['/'].includes(req.nextUrl.pathname);
+
+  if (isPublic && req.cookies.has('user')) {
+    return NextResponse.redirect(new URL('/home', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/', '/home'],
+};
