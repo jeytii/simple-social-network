5a11a144e0f81fbce486e553bf025766f241c9e6
Jonathan Tarnate <jstarnate16@gmail.com>
2022-08-26 13:42:44 +0800

    Add condition to fetch auth user in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,33 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import axios from 'lib/axios';
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   const isPublic = [
     '/',
     '/register',
     '/forgot-password',
     '/reset-password/:token',
+    '/verify/:token',
   ].includes(req.nextUrl.pathname);
+  const hasAuthToken = req.cookies.has('token');
 
-  if (isPublic && req.cookies.has('user')) {
+  if (isPublic && hasAuthToken) {
     return NextResponse.redirect(new URL('/home', req.url));
+  }
+
+  if (!isPublic && !hasAuthToken) {
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  if (!isPublic && hasAuthToken && !req.cookies.has('user')) {
+    const { data } = await axios.get('/private', {
+      params: {
+        authToken: req.cookies.get('token'),
+      },
+    });
+
+    req.cookies.set('user', data.data);
   }
 
   return NextResponse.next();
@@ -22,6 +39,7 @@ export const config = {
     '/register',
     '/forgot-password',
     '/reset-password/:token',
+    '/verify/:token',
     '/home',
   ],
 };
